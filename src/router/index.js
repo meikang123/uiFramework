b39c962b648fdff2d78daa0b7554949ca3413531
meikang <meikang@ztgame.com>
2020-08-28 16:25:43 +0800

    'ss'

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,55 @@ import { Transform } from '@framework/utils';
  * */
 import Layout from '@/layout';
 
+const componentGenerator = (Component, name) => {
+  console.log(Component);
+  return {
+    name,
+
+    render(h) {
+      return (
+        <instruction-tpl>
+          <gt-markdown-display content={Component} />
+        </instruction-tpl>
+      )
+    }
+  }
+}
+
+const frameworkData = require.context('@framework', true, /README.md$/);
+const d = {};
+frameworkData.keys().forEach(item => {
+  const paths = item.split('/').slice(1);
+  if(paths && paths[0] !== 'public' && paths.length > 1) {
+    const path = paths[0];
+    const name = paths[paths.length - 2];
+    console.log(paths, name);
+    if(!d[path]) {
+      d[path] = {
+        path: `/${path}`,
+        component: Layout,
+        children: []
+      };
+    }
+    d[path].children.push({
+      path: path === name ? 'index' : name,
+      component: componentGenerator(frameworkData(item), name),
+      name: name,
+      meta: { title: name, noCache: true }
+    })
+    if(d[path].children.length > 1) d[path].meta = { title: path, noCache: true }
+  }
+})
+const _routes = [Object.values(d)];
+
+console.log(_routes);
+
 /**
  * @description 动态引入路由模块, 根据模块内第一个路由meta{position:1}中的position进行菜单顺序调整
  * position 越大位置越靠后
  */
-const routeFiles = require.context('./modules', true, /\.js$/);
-const configRoutes = Transform.configRoutesToRoutes(routeFiles);
+/*const routeFiles = require.context('./modules', true, /\.js$/);
+const configRoutes = Transform.configRoutesToRoutes(routeFiles);*/
 
 const index = () => import(/* webpackChunkName: 'index' */ '@/views/index');
 
@@ -48,7 +91,7 @@ export const defaultRoutes = [{
  * 动态添加路由
  */
 export const authRoutes = [
-].concat(...defaultRoutes).concat(...configRoutes);
+].concat(...defaultRoutes).concat(..._routes);
 
 const router = new VueRouter({
   mode: 'history',
